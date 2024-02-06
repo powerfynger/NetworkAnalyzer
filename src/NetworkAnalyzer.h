b878bac659ad0ad82a046ed3fb2f659d93a219f8
powerfyng <puunika@mail.ru>
2024-02-06 16:29:22 -0500

    Initial commit

---
+++ src/NetworkAnalyzer.h
@@ -0,0 +1,22 @@
+#ifndef NETWORKANALYSER_H
+#define NETWORKANALYSER_H
+
+
+class FlowSaver
+{
+public:
+    void writeFlowToCSV();
+private:
+
+};
+
+class PacketAnalyser
+{
+public:
+    void analyzePacket();
+
+private:
+
+};
+
+#endif
